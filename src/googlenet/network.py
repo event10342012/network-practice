01d3db052921c86ff22befe99abc592f97111253
leochen <event10342012@gmail.com>
2020-08-16 22:24:37 +0800

    google net

---
+++ src/googlenet/network.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+
+class GoogleNet(tf.keras.Model):
+
+    def __init__(self):
+        super(GoogleNet, self).__init__()
+
+    def build(self, input_shape):
+        pass
+
+    def call(self, inputs, training=None, mask=None):
+        pass
+
+    def get_config(self):
+        pass
